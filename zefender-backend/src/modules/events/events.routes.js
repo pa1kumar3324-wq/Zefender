54ea1884348d3c8681bda5becfa11be5dfb03ee6
Mohit703-max <mohitbasavarajaiah@gmail.com>
2026-03-11 09:52:55 +0530

    removing old src, rebuilding clean architecture

--- zefender-backend/src/modules/events/events.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { triggerEvent } = require("./events.controller");
-const { verifyDeviceToken } = require("../../middleware/device.middleware");
-
-router.post("/", verifyDeviceToken, triggerEvent);
-
-module.exports = router;
--- zefender-backend/src/modules/playlists/playlists.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createPlaylist, getLatestPlaylist } = require("./playlists.controller");
-const { verifyToken } = require("../../middleware/auth.middleware");
-
-router.post("/", verifyToken, createPlaylist);
-router.get("/", verifyToken, getLatestPlaylist);
-
-module.exports = router;
